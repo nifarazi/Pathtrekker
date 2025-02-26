555f9106ccec981870e27f8022a3e157af91f2c6
nifarazi <nanjibafarazi@gmail.com>
2025-02-26 17:08:42 +0600

    Issue

--- src/main/java/com/example/pathtrekker/DatabaseConnection.java
+++ src/main/java/com/example/pathtrekker/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/register";
     private static final String USER = "root";
-    private static final String PASSWORD = "mirpurdohs832";
+    private static final String PASSWORD = "nanjiba@282002";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
